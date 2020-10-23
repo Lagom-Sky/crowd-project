acc61090816b932e40c25ae8bd8811a47e4b16e9
MR TANG <754752345@qq.com>
2020-10-23 09:38:56 +0800

    测试master分支的提交

--- Consumer/src/test/java/testVariable.java
+++ Consumer/src/test/java/testVariable.java
@@ -6,5 +6,6 @@ public class testVariable {
         int i ;
         System.out.println("hello 这里是test new branch");
         System.out.println("加油学会git指令");
+        System.out.println("测试master分支的提交策略");
     }
 }
